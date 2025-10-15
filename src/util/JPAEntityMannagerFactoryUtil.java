ae2660958e16e5b53c3b9fa18e4dc95f71de5ce5
vunge7 <vunge7.1990@gmail.com>
2025-10-15 06:21:50 +0100

    Update Cardex Geral

--- src/util/JPAEntityMannagerFactoryUtil.java
+++ src/util/JPAEntityMannagerFactoryUtil.java
@@ -45,7 +45,7 @@ public class JPAEntityMannagerFactoryUtil
 //                
 //                String ip = "localhost";
 
-                   //                String url = "jdbc:mysql://"   +ip +":3306/kitanda_db_1_moys_gulele_dvml_k11_joaquim?zeroDateTimeBehavior=convertToNull";
+                   //                String url = "jdbc:mysql://"   +ip +":3306/kitanda_db_moys_gulele_dvml_k11_joaquim?zeroDateTimeBehavior=convertToNull";
                     //                String password = "DoV90x?#";
                     //                
                     //                Map map = new HashMap();
@@ -81,7 +81,7 @@ public class JPAEntityMannagerFactoryUtil
                     String ip = informacao.get( 0 );
                     String porta = informacao.get( 1 );
 
-                    String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_1?zeroDateTimeBehavior=convertToNull";
+                    String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db?zeroDateTimeBehavior=convertToNull";
 
                     String user = "root";
                     String password = "DoV90x?#";
