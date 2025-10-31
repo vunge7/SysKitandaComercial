297862feded418b27914187e48e2b51f4b013ea0
vunge7 <vunge7.1990@gmail.com>
2025-10-31 09:14:47 +0100

    Updated

--- src/util/BDConexao.java
+++ src/util/BDConexao.java
@@ -106,7 +106,7 @@ public class BDConexao
             String ip = info.get( 0 ).trim();
             String porta = (info.size() > 1 ? info.get( 1 ).trim() : "3306");
 
-            String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_1_1"
+            String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db"
                     + "?zeroDateTimeBehavior=convertToNull"
                     + "&useSSL=false"
                     + "&allowPublicKeyRetrieval=true"
