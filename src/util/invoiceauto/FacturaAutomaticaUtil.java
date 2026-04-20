2fba64c064e7e663776595446b12a5ef8e57cca6
vunge7.1990@gmail.com <vunge7.1990@gmail.com>
2026-04-20 04:07:20 +0100

    update

--- src/util/invoiceauto/FacturaAutomaticaUtil.java
+++ src/util/invoiceauto/FacturaAutomaticaUtil.java
@@ -2,10 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package util;
+package util.invoiceauto;
 
 
 //import java.sql.Connection;
+import util.*;
 import comercial.controller.AnoEconomicoController;
 import comercial.controller.DadosInstituicaoController;
 import comercial.controller.DocumentosController;
