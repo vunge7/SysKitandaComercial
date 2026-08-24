f040be84b808a80ac5aebdbbebcba65cb9b37c84
Martinho Luis <martinhocanhongoluis@gmail.com>
2026-08-24 22:18:43 +0100

    Actual

--- src/util/BDConexao.java
+++ src/util/BDConexao.java
@@ -104,7 +104,7 @@ public class BDConexao
             String ip = info.get( 0 ).trim();
             String porta = (info.size() > 1 ? info.get( 1 ).trim() : "3306");
 
-            String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_gulele"
+            String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_lav_mor"
 
                     + "?zeroDateTimeBehavior=convertToNull"
                     + "&useSSL=false"
