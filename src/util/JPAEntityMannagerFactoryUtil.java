6f30003d1c541056ca51b2f020e15ccee997d40f
vunge7 <vunge7.1990@gmail.com>
2026-09-24 08:13:56 +0100

    updated

--- src/util/JPAEntityMannagerFactoryUtil.java
+++ src/util/JPAEntityMannagerFactoryUtil.java
@@ -53,7 +53,7 @@ public class JPAEntityMannagerFactoryUtil {
                     String porta = informacao.get(1);
 
 
-                    String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_gulele_golf?zeroDateTimeBehavior=convertToNull";
+                    String url = "jdbc:mysql://" + ip + ":" + porta + "/kitanda_db_gulele_golf_II?zeroDateTimeBehavior=convertToNull";
                   
                     String user = "root";
                     String password = "DoV90x?#";
